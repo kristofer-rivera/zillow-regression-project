dceebc152e2bca2e317f5bf1bb3df02556db730c
Kristofer Rivera <kristofer.e.rivera@gmail.com>
2022-03-28 23:43:35 -0500

    Initial commit of exploration notebook and python module.

---
+++ explore.py
@@ -0,0 +1,122 @@
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
+import statistics
+
+#Statistical analysis 
+def pearsonr(variable, target, alpha =.05):
+    corr, p = stats.pearsonr(variable, target)
+    print(f'The correlation value between the two variables is {corr:.4} and the P-Value is {p}.')
+    print('----------------------------------------------------------------------------')
+    if p < alpha:
+        print('Since the P value is less than the alpha, we reject the null hypothesis.')
+    else:
+        print('Since the P value is greater than the alpha, we fail to reject the null hypothesis.')
+
+def chi2(variable, target, alpha=.05):
+    observed = pd.crosstab(variable, target)
+    chi2, p, degf, expected = stats.chi2_contingency(observed)
+    print(f'The chi2 value between the two variables is {chi2} and the P-Value is {p}.')
+    print('----------------------------------------------------------------------------')
+    if p < alpha:
+        print('Since the P value is less than the alpha, we reject the null hypothesis.')
+    else:
+        print('Since the P value is greater than the alpha, we fail to reject the null hypothesis.')
+
+
+# Univariate exploration
+
+def plot_distributions(df):
+    for col in df.columns:
+        sns.histplot(x = col, data=df)
+        plt.title(col)
+        plt.show()
+
+# catergorical vs continuous
+
+def plot_variable_pairs(df):
+    sns.pairplot(df, kind='reg', plot_kws={'line_kws':{'color':'red'}})
+
+def plot_categorical_and_continuous_vars(df, cat_var, cont_var):
+    sns.barplot(data=df, y=cont_var, x=cat_var)
+    plt.show()
+    sns.boxplot(data=df, y=cont_var, x=cat_var)
+    plt.show()
+    sns.stripplot(data=df, y=cont_var, x=cat_var)
+    plt.show()
+
+## Bivariate Quant
+
+def plot_swarm(train, target, quant_var):
+    average = train[quant_var].mean()
+    p = sns.swarmplot(data=train, x=target, y=quant_var, color='lightgray')
+    p = plt.title(quant_var)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
+
+def plot_boxen(train, target, quant_var):
+    average = train[quant_var].mean()
+    p = sns.boxenplot(data=train, x=target, y=quant_var, color='lightseagreen')
+    p = plt.title(quant_var)
+    p = plt.axhline(average, ls='--', color='black')
+    return p
+
+## Bivariate Categorical
+
+def run_chi2(train, cat_var, target):
+    observed = pd.crosstab(train[cat_var], train[target])
+    chi2, p, degf, expected = stats.chi2_contingency(observed)
+    chi2_summary = pd.DataFrame({'chi2': [chi2], 'p-value': [p], 
+                                 'degrees of freedom': [degf]})
+    expected = pd.DataFrame(expected)
+    return chi2_summary, observed, expected
+
+def plot_cat_by_target(train, target, cat_var):
+    p = plt.figure(figsize=(2,2))
+    p = sns.barplot(cat_var, target, data=train, alpha=.8, color='lightseagreen')
+    overall_rate = train[target].mean()
+    p = plt.axhline(overall_rate, ls='--', color='gray')
+    return p
+
+### Multivariate
+
+def plot_all_continuous_vars(train, target, quant_vars):
+    '''
+    Melt the dataset to "long-form" representation
+    boxenplot of measurement x value with color representing the target variable. 
+    '''
+    my_vars = [item for sublist in [quant_vars, [target]] for item in sublist]
+    sns.set(style="whitegrid", palette="muted")
+    melt = train[my_vars].melt(id_vars=target, var_name="measurement")
+    plt.figure(figsize=(8,6))
+    p = sns.boxenplot(x="measurement", y="value", hue=target, data=melt)
+    p.set(yscale="log", xlabel='')    
+    plt.show()
+
+def plot_violin_grid_with_color(train, target, cat_vars, quant_vars):
+    cols = len(cat_vars)
+    for quant in quant_vars:
+        _, ax = plt.subplots(nrows=1, ncols=cols, figsize=(16, 4), sharey=True)
+        for i, cat in enumerate(cat_vars):
+            sns.violinplot(x=cat, y=quant, data=train, split=True, 
+                           ax=ax[i], hue=target, palette="Set2")
+            ax[i].set_xlabel('')
+            ax[i].set_ylabel(quant)
+            ax[i].set_title(cat)
+        plt.show()
+
+def plot_swarm_grid_with_color(train, target, cat_vars, quant_vars):
+    cols = len(cat_vars)
+    for quant in quant_vars:
+        _, ax = plt.subplots(nrows=1, ncols=cols, figsize=(16, 4), sharey=True)
+        for i, cat in enumerate(cat_vars):
+            sns.swarmplot(x=cat, y=quant, data=train, ax=ax[i], hue=target, palette="Set2")
+            ax[i].set_xlabel('')
+            ax[i].set_ylabel(quant)
+            ax[i].set_title(cat)
+        plt.show()
